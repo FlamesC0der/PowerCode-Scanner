4b43327223cb6f7d6b90f833e7211547264a6512
Alexey <alexm20games@gmail.com>
2023-10-27 13:04:34 +0300

    V2.0 Final

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { LogBox } from "react-native";
 import MainContainer from './navigation/MainContainer';
+
+LogBox.ignoreAllLogs()
 
 export default function App() {
   return (
